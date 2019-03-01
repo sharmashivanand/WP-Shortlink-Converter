d9f0b63f58f21ebae385e54c59df591bf9749d55
Shivanand Sharma <shivanand@binaryturf.com>
2019-03-01 15:05:49 +0530

    Update shortlink-converter.php

--- shortlink-converter.php
+++ shortlink-converter.php
@@ -15,19 +15,33 @@
  */
 
 function bt_replace() {
+    if(!is_user_logged_in())  return;
+    if(!is_main_query())  return;
 	$post_id = get_the_id();
 	if ( 'page' == get_post_type($post_id) || 'post' == get_post_type($post_id)) { // If we are on a post or a page (we want to avoid attachments etc.)
 		$content = get_the_content(); // get the unfiltered content
 		$siteurl = get_site_url(); // get the address of the current site
 		$siteurl = trailingslashit($siteurl);
 		$siteurl = preg_quote($siteurl); // 
-		$exp = "/href=[\"|']($siteurl[^\"']+)/";
+        $exp = '~href=["\'](' . $siteurl . '[^\'"]+)~';
+        
+        //$ruchika = preg_quote('ruchi:ka');
+
+        //$text = '<a href="https://maryannjacobsen.com/general/news/">anchor</a>';
+        //$exp = "/($ruchika)/";
+        //bt_log($exp);
+        //bt_log( '<p class="clear">'.$post_id.'</p>');
+        //return;
 		
-		$content = preg_replace_callback($exp, 'bt_replace_url', $content); // replacement magic. Match urls begining with our WP site.	
+        $content = preg_replace_callback( $exp, 'bt_replace_url', $content ); // replacement magic. Match urls begining with our WP site.	
+
+        //$content = str_replace('www.maryannjacobsen.com','maryannjacobsen.com', $content);
+        //$content = str_replace('http://maryannjacobsen.com','https://maryannjacobsen.com', $content);
 
 		if(is_user_logged_in()) { 
 			wp_update_post( array('ID' => $post_id, 'post_content' => $content) ,$error); // update the post with the new content
-			bt_log($error);
+            bt_log($error);
+            //bt_log($content);
 		}
 	}
 }
